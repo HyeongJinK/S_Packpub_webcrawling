e74bb57f599e2f14dde53d0d176b2fe9b5b0ee18
estgames <wkzkfmxk23@gmail.com>
2018-04-06 17:58:02 +0900

    수정

--- app.js
+++ app.js
@@ -3,19 +3,7 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var request = require("request");  
 var cheerio = require("cheerio"); 
-var jsdom = require('jsdom');
-var $ = require("jQuery");
 
-jsdom.env({
-  url: 'http://news.ycombinator.com',
-  src: [],
-  done: function (err, window) {
-    var $ = jquery(window);
-    $('td.title:not(:last) a').each(function () {
-      console.log(' -', $(this).text());
-    });
-  }
-});
 
 //var url = "https://ansuchan.com";
 var url = "https://www.packtpub.com/all-books?search=&availability_list%5BAvailable%5D=Available&offset=&rows=48&sort=&theme_raw=true";
@@ -23,23 +11,12 @@ var url = "https://www.packtpub.com/all-books?search=&availability_list%5BAvaila
 request(url, function(error, response, body) {  
   if (error) throw error;
 
-  //console.log(body);
-  //console.log($(body))
-  //  $("div.book-block-outer [data-product-id]")
-   // var metadata = JSON.parse(body);
-  //  var menudata = metadata.data.tableOfContents;
-
-  //  console.log(menudata[0].children[0]);
-    //console.log(metadata.data.tableOfContents[0]);
-
-
-
-//
-//   var $ = cheerio.load(body);
-//   var postElements = $("section.posts article.post");
-//   postElements.each(function() {
+   var $ = cheerio.load(body);
+   var postElements = $("div.book-block-outer[data-product-id]");
+   postElements.each(function() {
+console.log($(this).attr("data-product-id"));
 //     var postTitle = $(this).find("h1").text();
 //     var postUrl = $(this).find("h1 a").attr("href");
 //     console.log(postTitle);
-//   });
+   });
 });
