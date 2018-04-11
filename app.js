4539b0a3164672488995740ffd7c578dfd6bb80f
estgames <wkzkfmxk23@gmail.com>
2018-04-11 14:49:58 +0900

    여기서는 안되나?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //packpub.com 웹크롤링
-
+//TODO isbn 목록 가져오기 적용.. 파싱 적용
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var request = require("request");  
 var cheerio = require("cheerio"); 
@@ -25,6 +25,7 @@ request(menuUrl, function(error, response, body) {
     mParserData.data.tableOfContents.forEach(element => {
       var parentID = element.id
       var baseContentUrl = "https://www.packtpub.com/mapt-rest/users/me/products/"+isbn+"/chapters/"+parentID;
+      var ch_index = 0;
       element.children.forEach(element => {
         var contentUrl;
         if (parentID != element.id) {
@@ -38,7 +39,7 @@ request(menuUrl, function(error, response, body) {
         var options = {
             url : contentUrl
             , headers: {//TODO 키 확인
-                "Authorization" : "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZmNmNGI4MC0zNTBkLTRmZjEtODRlOC1jNWY4NWM2YmYzYzgiLCJ1c2VybmFtZSI6IndremtmbXhrMjNAZ21haWwuY29tIiwicGVybWlzc2lvbnMiOltdLCJpYXQiOjE1MjMzNDU0MDksImV4cCI6MTUyMzM0OTAwOX0.YqKkSI6JzdFSZzDG4WiwUz0mOzBftvpE7a5V43BwYUNQ9BKlhP0qMFz9-wBdrATmaBXmn2rcBN3lkwqqQHrbDsMb6UI9mP3IfZLIbl9BW_4XFzbumnbuGnmPlkFtMiJ_R18RY_FGmYXTaqARjbljNRAmpkdv0eTgHiM0XcuEMqdfON1FkT-Iuacta4gtx8JVTDJQ2woGQoc1JSe5T70_IxOGUEyLXpycFHQSbmOPXe4ctqVy78ooJG12dFpSO77meNp0Tf_pTIY3ByTZrNQHfe02I31YDsytQsblmOzwayg6fHjgiSRkV7jcnBDmkgc4sJUwkA_h7eSWHxIfOI2z3g"
+                "Authorization" : "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZmNmNGI4MC0zNTBkLTRmZjEtODRlOC1jNWY4NWM2YmYzYzgiLCJ1c2VybmFtZSI6IndremtmbXhrMjNAZ21haWwuY29tIiwicGVybWlzc2lvbnMiOltdLCJpYXQiOjE1MjM0MjU0OTAsImV4cCI6MTUyMzQyOTA5MH0.dFONCD2rlRSPQG0qy2rhCM-mhYcwBSRl3wB-_RAwhAA8Z6gfRUYCHn4t9dwHEgN5BveI4SvBO09PtD_J7eLrz3oup1-8s7Hj6RYz2jE_dYcefE_xKrxw7nbmX_JJBYM1gbsmPDQHlYEBS7VK__5CnX6avA0qZWIA0voWcHdfvijHC5Wt_cYhWDtlk52_Nx9ccgHvQ-AW3Aq-wgMtxHk8e9bpvgxpwSZlRiZ0V4R5iYzUpYU-Ubv2nX1c4GRqN3jbQy9-PImwSo03fWpgTiDmJ-aHQVplXT-npGPoA45Ny2fnJs66-xbV7YXzTipsjm490u06enbypMtYo-IkMieu3A"
           }
         }
         //console.log(options)
@@ -50,11 +51,13 @@ request(menuUrl, function(error, response, body) {
       
           if (cParserData.status === 'success') {
               if (cParserData.data.entitled) {
+                ch_index++;
+                fs.writeFileSync(bookPath+"/"+parentID+"_"+ch_index+"_"+element.title, cParserData.data.content);
                 //TODO html 파싱
-                let doc = new PDFDocument();  //TODO 파일 순서에 맞게 이름 조정 필요
-                doc.pipe(fs.createWriteStream(bookPath+"/"+parentID+"_"+element.title));
-                doc.text(cParserData.data.content);
-                doc.end();
+                // let doc = new PDFDocument();  //TODO 파일 순서에 맞게 이름 조정 필요
+                // doc.pipe(fs.createWriteStream(bookPath+"/"+parentID+"_"+element.title));
+                // doc.text(cParserData.data.content);
+                // doc.end();
                   //console.log(cParserData.data.content);
               }
           }
--- content.js
+++ content.js
@@ -11,13 +11,13 @@ var url = "https://www.packtpub.com/mapt-rest/users/me/products/9781786462558/ch
 var options = {
     url : "https://www.packtpub.com/mapt-rest/users/me/products/9781786462558/chapters/1/sections/ch01lvl1sec10"
     , headers: {
-        "Authorization" : "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZmNmNGI4MC0zNTBkLTRmZjEtODRlOC1jNWY4NWM2YmYzYzgiLCJ1c2VybmFtZSI6IndremtmbXhrMjNAZ21haWwuY29tIiwicGVybWlzc2lvbnMiOltdLCJpYXQiOjE1MjMzNDU0MDksImV4cCI6MTUyMzM0OTAwOX0.YqKkSI6JzdFSZzDG4WiwUz0mOzBftvpE7a5V43BwYUNQ9BKlhP0qMFz9-wBdrATmaBXmn2rcBN3lkwqqQHrbDsMb6UI9mP3IfZLIbl9BW_4XFzbumnbuGnmPlkFtMiJ_R18RY_FGmYXTaqARjbljNRAmpkdv0eTgHiM0XcuEMqdfON1FkT-Iuacta4gtx8JVTDJQ2woGQoc1JSe5T70_IxOGUEyLXpycFHQSbmOPXe4ctqVy78ooJG12dFpSO77meNp0Tf_pTIY3ByTZrNQHfe02I31YDsytQsblmOzwayg6fHjgiSRkV7jcnBDmkgc4sJUwkA_h7eSWHxIfOI2z3g"
+        "Authorization" : "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZmNmNGI4MC0zNTBkLTRmZjEtODRlOC1jNWY4NWM2YmYzYzgiLCJ1c2VybmFtZSI6IndremtmbXhrMjNAZ21haWwuY29tIiwicGVybWlzc2lvbnMiOltdLCJpYXQiOjE1MjM0MjU0OTAsImV4cCI6MTUyMzQyOTA5MH0.dFONCD2rlRSPQG0qy2rhCM-mhYcwBSRl3wB-_RAwhAA8Z6gfRUYCHn4t9dwHEgN5BveI4SvBO09PtD_J7eLrz3oup1-8s7Hj6RYz2jE_dYcefE_xKrxw7nbmX_JJBYM1gbsmPDQHlYEBS7VK__5CnX6avA0qZWIA0voWcHdfvijHC5Wt_cYhWDtlk52_Nx9ccgHvQ-AW3Aq-wgMtxHk8e9bpvgxpwSZlRiZ0V4R5iYzUpYU-Ubv2nX1c4GRqN3jbQy9-PImwSo03fWpgTiDmJ-aHQVplXT-npGPoA45Ny2fnJs66-xbV7YXzTipsjm490u06enbypMtYo-IkMieu3A"
     }
 }
 
 request(options, function(error, response, body) {  
     if (error) throw error;
-    //console.log("adfasdf = " +body);
+    console.log("adfasdf = " +body);
     var parserData = JSON.parse(body);
     
     if (parserData.status === 'success') {
