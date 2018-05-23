4b2d5bb16f4b3153c0396331d1ecc0c1c374ad22
김형진 <wkzkfmxk23@gmail.com>
2018-05-23 15:54:31 +0900

    down

--- app-sync.js
+++ app-sync.js
@@ -12,9 +12,9 @@ var request = require("sync-request");
 var fs =require('fs');
 
 let downloadDataPath = "./download/"
-const user = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkMDBjODk3NC05OTNjLTQ2MzItODQzMS04NjY3ZmU4ZTJhOWMiLCJ1c2VybmFtZSI6InhwdG14bWRrZWwxMkBnbWFpbC5jb20iLCJwZXJtaXNzaW9ucyI6W10sImlhdCI6MTUyNjgxMTk1OSwiZXhwIjoxNTI2ODE1NTU5fQ.HEsZAlRe3eSZ4ce4OOk8m1rkOTi-YJcPwXOqYDtbLI2FUgNy4DUzkKfoYIZjs6VnZN9DsxFCDA2HGMTv7PCzgrwBCYRh8kOV-0dB2Qdlt4fzXr-XzwQH_aVJL9kRglonugN-Jsmq9Nq4TUQwtbQq7Yn1B71aVoYb5nFxxCdYEgCK8O81lpfdE_YomCncNM3lDqjI2QfEaSuUpP7EyITQ6lAXDZRNyxcWWwtLG0fdtATar1Ic6F_CYUBeWuaois4fg_K_41cHubpJ7t9mor0wIClpfuv9l7_H0Rq0aABnVaQfzGGwzagDGr3AbwylqsNFnQ-dNx4TZbDDfUYaSMfs1Q";
+const user = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkMDBjODk3NC05OTNjLTQ2MzItODQzMS04NjY3ZmU4ZTJhOWMiLCJ1c2VybmFtZSI6InhwdG14bWRrZWwxMkBnbWFpbC5jb20iLCJwZXJtaXNzaW9ucyI6W10sImlhdCI6MTUyNzA1Njc4MywiZXhwIjoxNTI3MDYwMzgzfQ.XiLCv_YKKWXvcriYI7oi4ZbbiU93yDxhsFH3hO8W7Zsvz-d_JRftHZ-_YQxKDYZG4wfm7EB7JXxZmwu2so0kutOQfOwu6QKBvvXu_UQBZ6mUYHj95O-EsX9ZGB-wBRJquiseXVR19vfX6JU9LIclj7oZ5N8XYrYg53iu3Yci3Zefm17t1rXlhYv8dpaquhde3pqpXQsNPPkyeOEeSEOswxS-Q7k2gRsInv5DzE4-Hhkc7_qYjlRGddLzErfH_5IT3YzSV5xZM266Is9NBkufUoMaSQtF56sEj0rZDWB_cilvF19d8P-xg9tFXtG-4RXanTqmtw1h16gUqN0a-erM2w";
 
-let data = fs.readFileSync("./downlist/isbn_Python.txt", 'utf8')
+let data = fs.readFileSync("./downlist/isbn_JavaScript.txt", 'utf8')
     
 let isbns = data.split("\t")
 
@@ -59,7 +59,7 @@ for (i in isbns) {
                         if (contentParserData.data.entitled) {
                             fs.writeFileSync(bookPath+"/"+parentID+"_"+element.index+"_"+replace(element.title)+".html", contentParserData.data.content);
                             console.log(parentID+"_"+element.index+"_"+element.title)
-                            sleep(200);
+                            //sleep(200);
                         } else {
                             fs.writeFileSync(bookPath+"/"+parentID+"_"+element.index+"_"+replace(element.title)+"_demo.txt", contentParserData.data.content);
                             console.log(parentID+"_"+element.index+"_"+element.title+"_demo")
