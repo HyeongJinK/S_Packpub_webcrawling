3f3c845de7266130b702ed0be0840f141d8d7904
estgames <wkzkfmxk23@gmail.com>
2018-08-10 16:00:20 +0900

    디렉토리, 파일 만들기

--- translate_mkdir.js
+++ translate_mkdir.js
@@ -10,21 +10,41 @@ let param = [
     "9781786461407"
 ]
 
-db.get("SELECT * FROM book WHERE isbn = ?", param, function(err, rows) {    
-    if (!fs.existsSync(gitbookPath)) {
-        fs.mkdirSync(gitbookPath, function(err) {
+function createDir(path) {
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path, function(err) {
             console.log(err);
         })
     }
+}
 
-    let bookPath = gitbookPath +"/"+ replace(rows.title);
-    if (!fs.existsSync(bookPath)) {
-        fs.mkdirSync(bookPath, function(err) {
-            console.log(err);
-        })
-    }
+function createMD(path, fileName, content) {
+    fs.writeFile(path+'/'+fileName, content, function(err) {
+        if(err) throw err;
+        console.log(fileName+' File write completed');
+      });
+}
 
+db.get("SELECT * FROM book WHERE isbn = ?", param, function(err, rows) {  
+    createDir(gitbookPath);
+
+    let defaultPath = gitbookPath +"/"+ replace(rows.title);
+    let draftPath = defaultPath +"/_draft"
+    createDir(defaultPath);
+    createDir(draftPath);
+    
+    createMD(defaultPath, "README.md", "");
+    createMD(defaultPath, "SUMMARY.md", "");
+    
+    let step = -999;
     db.all("SELECT * FROM content WHERE isbn = ? order by contentIndex", param, function(err, rows) {
-        console.log(rows);
+        rows.forEach(function(value, index, array) {
+            if (step != value.menuNum) {
+                step = value.menuNum;
+                createDir(draftPath+"/ch"+step);
+            }
+            
+            createMD(draftPath+"/ch"+step, step+"_"+value.contentIndex+".md", "");
+        });
     });
 });
