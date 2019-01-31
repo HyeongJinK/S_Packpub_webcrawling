8a256833f356b90cf7159bec9d823632a68714e7
napasun <wkzkfmxk23@gmail.com>
2019-01-31 09:08:50 +0900

    merge

--- image.js
+++ image.js
@@ -3,15 +3,20 @@ var request = require("sync-request");
 var db = new sqlite3.Database('./DB/books.db');
 let p = 0;
 //          
-db.all("SELECT isbn FROM book WHERE imgUrl = '' or imgUrl ISNULL AND category = 'Application Development'", function(err, rows) {
+db.all("SELECT isbn FROM book WHERE imgUrl = '' or imgUrl ISNULL", function(err, rows) {
     rows.forEach(function (row) {
         let isbn = row.isbn;
         //console.log(isbn);
         let menuUrl = "https://www.packtpub.com/mapt-rest/products/"+isbn+"/metadata";
         //console.log(menuUrl);
-        let menuData = request("GET", menuUrl)
-        let menuParserData = JSON.parse(menuData.getBody());
-        let imgUrl = menuParserData.data.imageUrl;
+        let imgUrl = ""
+        try {
+            let menuData = request("GET", menuUrl)
+            let menuParserData = JSON.parse(menuData.getBody());
+            imgUrl = menuParserData.data.imageUrl;
+        } catch(e) {
+            imgUrl = "#";
+        }
         //console.log(imgUrl);
         db.serialize(function() {
             let stmt = db.prepare("UPDATE  book SET imgUrl = ? WHERE isbn = ?");
