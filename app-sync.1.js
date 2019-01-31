be4e882fb8c28012f81fb1ead0f26c1e9e74bd62
napasun <wkzkfmxk23@gmail.com>
2019-01-31 15:30:24 +0900

    new book

--- app-sync.1.js
+++ app-sync.1.js
@@ -14,14 +14,14 @@ var request = require("sync-request");
 var fs =require('fs');
 
 let downloadDataPath = "./download/"
-const user = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YmI2Zjk3ZC1iMDRlLTRhYmYtODc5NC04ZmEyYmM5ZjAwMzciLCJ1c2VybmFtZSI6InBhY2twdWIxQGdtYWlsLmNvbSIsInBlcm1pc3Npb25zIjpbXSwic3Vic2NyaXB0aW9uIjpbImFsbCJdLCJwZXJtcyI6IkFBQUFBQUFBQUFBQUFBQUFBQUE9IiwiaWF0IjoxNTQ4ODM0MTAyLCJleHAiOjE1NDg4Mzc3MDJ9.wlYAQgp9bCVHXZDqax-xC9MS_aFgpAGxvfk0DedfW-5L_jiZBK7Hw2x5t3FdvYDm1mF7E4s8R_g_ZTu7Ki4SPY3b9xRwJeuV0hdUs19eD1t3SoJNqOGqX7gznpiLx99qMPbEF24c9k4T8BX1ID_ZGDXRWYN498oIkD4me2xYkIPYVuFPZp138EmF_1Bv1Ix4HgUxPUHcxuq1mksDnx8LqywjP9Hi6TIIiOfU87nLtosggdzmYF75qapn-Q-MqODVLLMTyoyFgBpjh_94hbLnyf93XPLyMy9AgunpSZYlri5FN5s9Q8Lk-7x-5ld_upmd1yTApLmMc9ffnOa3O7osag";
+const user = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YmI2Zjk3ZC1iMDRlLTRhYmYtODc5NC04ZmEyYmM5ZjAwMzciLCJ1c2VybmFtZSI6InBhY2twdWIxQGdtYWlsLmNvbSIsInBlcm1pc3Npb25zIjpbXSwic3Vic2NyaXB0aW9uIjpbImFsbCJdLCJwZXJtcyI6IkFBQUFBQUFBQUFBQUFBQUFBQUE9IiwiaWF0IjoxNTQ4OTA4OTk0LCJleHAiOjE1NDg5MTI1OTR9.ZP5-TiihVrGq6DWMmK3BYvG0k2BEEGDm8PCSB4wEhHG7isqkKKuIQ2qBijpQIIh0KzYuqj3bZ2vH9UZUkLB55xne8ldd1Su-oMTJoZe7_RZHZAx6qlUiytGcOV2h7o-pMtP0Glv4-XXv6vG8CwcsVM9--xg34jlsJLnyP31-Qn_yHfcmgnkh2QagVquomG80NwAT95-wHXYO23-PqbbzyJOC-vhSQNnCgnkwEbOFs_LK6QCo1UW3njhck83NHZkllIYWB5gFu1WbyGocVUF-X_znMdIZkh5P2lCjx7_3xFDQoO4IjZ-sOHNX2TjZcKQXRYvKRISMFKOYmW3Us6ixTA";
 let me = request("GET", "https://services.packtpub.com/users-v1/users/me/metadata", {
 	headers: {
 		"Authorization" : user
 }});
 me.getBody("utf-8");
 
-let isbn = 9781788997270
+let isbn = 9781789532470
 	//https://www.packtpub.com/mapt-rest/products/9781789615265/metadata
 let menuUrl = "https://www.packtpub.com/mapt-rest/products/"+isbn+"/metadata";
 
@@ -46,7 +46,7 @@ if (menuParserData.data.title.indexOf("[Video]") == -1) {
 			let contentData;
 			let contentParserData;
 			element.children.forEach(element => {
-				if (element.index > 57) {
+				if (element.index > 160) {
 					if (parentID != element.id) {
 						contentUrl = baseContentUrl +"/"+ element.id
 					} else {
